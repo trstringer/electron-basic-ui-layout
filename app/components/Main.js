7b813c71213bb094566456304a54337bca155722
Thomas Stringer <tr.stringer@outlook.com>
2016-09-06 10:36:44 -0400

    added back in the appbar and statusbar components

--- app/components/Main.js
+++ app/components/Main.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import styles from './Main.css';
 import MainLeft from './MainLeft';
 import MainRight from './MainRight';
-
+import StatusBar from './StatusBar';
+import AppBar from './AppBar';
 
 export default class Main extends Component {
   render() {
     return (
       <div>
         <div className={styles.container}>
+          <AppBar />
           <MainLeft />
           <MainRight />
+          <StatusBar />
         </div>
       </div>
     );
